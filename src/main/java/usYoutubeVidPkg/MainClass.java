98349b3cebce604e0ad0ebd447e2c7f0199ca3bf
saifeldeenmohamed <saif.mohamed2020@gmail.com>
2021-07-09 21:58:17 +0200

    YoutubeVid_Commit1

---
+++ src/main/java/usYoutubeVidPkg/MainClass.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package usYoutubeVidPkg;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+import java.io.IOException;
+import static usYoutubeVidPkg.YoutubeTitleWordCount.tagsFn;
+import static usYoutubeVidPkg.YoutubeTitleWordCount.titlesFn;
+
+/**
+ *
+ * @author SaiF El-deen
+ */
+public class MainClass {
+   
+    public static void main(String[] args) throws IOException {
+        
+        Logger.getLogger ("org").setLevel (Level.ERROR);
+        // CREATE SPARK CONTEXT
+        SparkConf conf = new SparkConf ().setAppName ("wordCounts").setMaster ("local[3]");
+        JavaSparkContext sparkContext = new JavaSparkContext (conf);
+        // LOAD DATASETS
+        JavaRDD<String> videos = sparkContext.textFile ("src/main/resources/USvideos.csv");
+        
+        
+        
+        // PRINTING
+        System.out.println("titles =");
+        titlesFn(videos);
+        System.out.println("tags =");
+        tagsFn(videos);
+        
+        
+    }
+
+    
+    
+        
+}
+
---
+++ src/main/java/usYoutubeVidPkg/YoutubeTitleWordCount.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package usYoutubeVidPkg;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.spark.api.java.JavaRDD;
+
+/**
+ *
+ * @author SaiF El-deen
+ */
+public class YoutubeTitleWordCount {
+    
+    private static final String COMMA_DELIMITER = ",";
+    
+    public static String extractTitle(String videoLine) {
+        try {
+            return videoLine.split (COMMA_DELIMITER)[2];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            return "";
+        }
+    }
+    public static String extractTag(String videoLine) {
+        try {
+            return videoLine.split (COMMA_DELIMITER)[6];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            return "";
+        }
+    }
+    
+    public static void titlesFn(JavaRDD<String> videos) {
+
+    //-----------------------------------Exctact title ---------------------------
+        JavaRDD<String> titles = videos
+                                        .map (YoutubeTitleWordCount::extractTitle)
+                                        .filter (StringUtils::isNotBlank);
+        
+        
+       // JavaRDD<String>
+        JavaRDD<String> words = titles.flatMap (title -> Arrays.asList (title
+                                                                            .toLowerCase ()
+                                                                            .trim ()
+                                                                            .replaceAll ("\\p{Punct}", " ")
+                                                                            .split (" ")).iterator ());
+        System.out.println(words.toString ());
+        // COUNTING
+        Map<String, Long> wordCounts = words.countByValue ();
+        List<Map.Entry> sorted = wordCounts.entrySet ().stream ()
+                                                       .sorted (Map.Entry.comparingByValue ())
+                                                       .collect (Collectors.toList ());
+        // DISPLAY
+        for (Map.Entry<String, Long> entry : sorted) 
+        {
+            System.out.println (entry.getKey () + " : " + entry.getValue ());
+        }
+    }
+    
+    
+    
+    public static void tagsFn(JavaRDD<String> videos) {
+        //-----------------------------------Exctact tag ---------------------------
+        JavaRDD<String> tags = videos
+                                    .map (YoutubeTitleWordCount::extractTag)
+                                    .filter (StringUtils::isNotBlank);
+        
+        // JavaRDD<String>
+        JavaRDD<String> words = tags.flatMap (tag -> Arrays.asList (tag
+                                                                        .toLowerCase ()
+                                                                        .split ("\\|")).iterator ())
+                                                                        .filter (StringUtils::isNotBlank);
+        System.out.println(words.toString ());
+        
+        // COUNTING
+        Map<String, Long> wordCounts = words.countByValue ();
+        List<Map.Entry> sorted = wordCounts.entrySet ().stream ()
+                                                       .sorted (Map.Entry.comparingByValue ())
+                                                       .collect (Collectors.toList ());
+        // DISPLAY
+        for (Map.Entry<String, Long> entry : sorted) 
+        {
+            System.out.println (entry.getKey () + " : " + entry.getValue ());
+        }
+    }
+    
+    
+    
+}
